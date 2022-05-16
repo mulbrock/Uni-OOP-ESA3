d4e0b2db4879f644e011e5a23375f4053587bddc
Hendrik <hendrikmuehlenbrock@gmx.de>
2022-05-16 15:30:47 +0200

    Added: Tower upgrade costs and show range

--- entities/towers/tower.py
+++ entities/towers/tower.py
@@ -12,8 +12,16 @@ class Tower(Entity):
         self.build_cost = _cost
 
         self.range = _range
+        self.range_upgrade_level = 0
+        self.range_upgrade_cost = 1
+
         self.cool_down_time = _cool_down
+        self.speed_upgrade_level = 0
+        self.speed_upgrade_cost = 1
+
         self.attack_power = _attack_power
+        self.attack_power_upgrade_level = 0
+        self.attack_power_upgrade_cost = 1
 
         self.timer = time.time()
         self.aimed_enemy = None
@@ -51,12 +59,27 @@ class Tower(Entity):
 
     def upgrade_range(self):
         self.range += 5
+        self.range_upgrade_level += 1
+        self.range_upgrade_cost += 2
+
+    def get_upgrade_range_cost(self):
+        return self.range_upgrade_cost
 
     def upgrade_power(self):
         self.attack_power += 1
+        self.attack_power_upgrade_level += 1
+        self.attack_power_upgrade_cost += 2
+
+    def get_upgrade_power_cost(self):
+        return self.attack_power_upgrade_cost
 
     def upgrade_speed(self):
         self.cool_down_time -= 0.01
+        self.speed_upgrade_level += 1
+        self.speed_upgrade_cost += 2
+
+    def get_upgrade_cost(self):
+        return self.speed_upgrade_cost
 
     def draw_range(self, win):
         pygame.draw.circle(win, [0, 200, 200, 50], self.get_center(), self.range, 2)
